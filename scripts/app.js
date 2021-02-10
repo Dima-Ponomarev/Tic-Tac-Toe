4ffafa68c448a51a1a11ed9adc3216051eb19bcd
Dima Ponomarev <kikkyow18@gmail.com>
2021-02-10 17:37:45 +0400

    Add core game logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 const Player = (sign) =>{
     this.sign = sign;
-
+    let occupiedTiles = [];
     const getSign = () => {
         return sign;
     };
 
-    return {getSign};
+    return {occupiedTiles, getSign};
 }
 
 const gameBoard = (() => {
@@ -29,7 +29,7 @@ const displayController = (() => {
 
     gameBoardDOM.forEach((gameTile) => {
         gameTile.addEventListener('click', (e) =>{
-            if (e.target.textContent !== '') return;
+            if (e.target.textContent !== '' || gameController.getGameStatus()) return;
             gameController.playRound(parseInt(e.target.dataset.index));
             updateGameboard();
         })
@@ -48,16 +48,59 @@ const gameController = (() => {
     const playerX = Player('X');
     const playerO = Player('O');
     let round = 0;
+    let gameIsOver = false;
 
     const playRound = (tileIndex) =>{
-        gameBoard.setTile(tileIndex, getCurrentPlayerSign());
+        const player = getCurrentPlayer();
+        player.occupiedTiles.push(tileIndex);
+        gameBoard.setTile(tileIndex, player.getSign());
+
+        console.log(player.occupiedTiles);
+        const winner = checkWinner(player.occupiedTiles);
+        console.log(winner)
+        if (checkWinner(player.occupiedTiles)){
+            gameIsOver = true;
+            console.log(`player ${player.getSign()} won`)
+        }
+
+        if (round === 8) {
+            gameIsOver = true;
+            console.log('draw', player.occupiedTiles)
+        }
         round++;
     } 
 
-    const getCurrentPlayerSign = () =>{
-        return round % 2 === 1 ? playerO.getSign() : playerX.getSign();
+    const getCurrentPlayer = () =>{
+        return round % 2 === 1 ? playerO : playerX;
     }
+
+    const checkWinner = (occupiedTiles) => {
+        let status = false;
+        winConditions =[
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        winConditions.forEach(winCondition => {
+            if (winCondition.every(index => occupiedTiles.includes(index))){
+                status = true;
+            }
+        });
+        return status;
+    }
+
+    const getGameStatus = () =>{
+        return gameIsOver;
+    }
+
     return{
-        playRound
+        playRound,
+        getGameStatus
     }
 })();
