fd95ac9a8c52f25916667bf294a0bca772b59285
Dima Ponomarev <kikkyow18@gmail.com>
2021-02-10 15:01:15 +0400

    Add gameController and displayController modules

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,62 @@ const Player = (sign) =>{
     this.sign = sign;
 
     const getSign = () => {
-        return this.sign;
+        return sign;
     };
 
     return {getSign};
 }
 
 const gameBoard = (() => {
-    const gameboard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'O'];
+    const gameboard = ['', '', '', '', '', '', '', '', ''];
     const setTile = (index, sign) =>{
         gameboard[index] = sign;
     }
 
+    const getTile = (index) =>{
+        return gameboard[index]; 
+    }
+
     return {
-        setTile
+        setTile,
+        getTile
     }
 })();
 
+const displayController = (() => {
+    const gameBoardDOM = document.querySelectorAll('.game-tile');
+
+    gameBoardDOM.forEach((gameTile) => {
+        gameTile.addEventListener('click', (e) =>{
+            if (e.target.textContent !== '') return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateGameboard();
+        })
+    })
+    const updateGameboard = () =>{
+        gameBoardDOM.forEach((tile, index) =>{
+            tile.textContent = gameBoard.getTile(index);
+        })
+    }
+    return{
+        updateGameboard
+    }
+})();
+
+const gameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 0;
+
+    const playRound = (tileIndex) =>{
+        gameBoard.setTile(tileIndex, getCurrentPlayerSign());
+        round++;
+    } 
+
+    const getCurrentPlayerSign = () =>{
+        return round % 2 === 1 ? playerO.getSign() : playerX.getSign();
+    }
+    return{
+        playRound
+    }
+})();
